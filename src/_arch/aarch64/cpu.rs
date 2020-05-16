acc96613f9728c11f504a8e13259ab3c7771e2bc
Goran Peretin <goran.peretin@gmail.com>
2020-05-16 13:25:12 -0700

    Add more documentation for stuff from chapter 01.

--- src/_arch/aarch64/cpu.rs
+++ src/_arch/aarch64/cpu.rs
@@ -2,11 +2,14 @@
 global_asm!(include_str!("cpu.S"));
 
 /// Pause execution on the core.
+// This function is called by the panic handler, we could have just as easily
+// defined this as a noop handler, by having just `loop {}` in the method body
 #[inline(always)]
 pub fn wait_forever() -> ! {
     unsafe {
         loop {
-            // This macro is documented here https://doc.rust-lang.org/nightly/unstable-book/library-features/llvm-asm.html
+            // This macro is documented here:
+            // https://doc.rust-lang.org/nightly/unstable-book/library-features/llvm-asm.html
             // First (and only required) argument is called assembly template and I think it
             // represents the command, or a piece of assemby code to execute.
             llvm_asm!("wfe"
@@ -18,5 +21,3 @@ pub fn wait_forever() -> ! {
         }
     }
 }
-
-
--- src/panic_wait.rs
+++ src/panic_wait.rs
@@ -3,6 +3,8 @@
 use crate::cpu;
 use core::panic::PanicInfo;
 
+// We are required to define a custom panic handler if we're using #![no_std]
+// ! as return type implies that this function never returns.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     cpu::wait_forever()
